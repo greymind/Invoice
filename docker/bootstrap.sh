6dd3a8a31043b92f6676785682e90cc27177b489
Balakrishnan (Balki) Ranganathan <balki@live.com>
2015-08-13 05:52:55 -0700

    Cleaned up test bootstrap

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,12 +1,1 @@
 #!/usr/bin/env bash
-
-# MongoDB
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-apt-get update
-apt-get install -y mongodb-org
-
-#if ! [ -L /var/www ]; then
-#  rm -rf /var/www
-#  ln -fs /vagrant /var/www
-#fi
